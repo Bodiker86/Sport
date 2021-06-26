a140cec2d7b723226236a4506b2420179f855203
Bodiker86 <80534899+Bodiker86@users.noreply.github.com>
2021-06-26 23:20:54 +0200

    php

--- bmi.php
+++ bmi.php
@@ -10,6 +10,12 @@
 </head>
 
 <body>
+    
+    <?php
+
+    $db = new mysqli ('localhost','root', '', 'egzamin')
+    ?>
+
     <div id="naglowek">
         <div id="logo">
             <img src="wzor.png" alt="wzór BMI">
@@ -20,13 +26,25 @@
         </div>
     </div>
 
-    <div id="gluwny">
+    <div id="glowny">
         <table>
             <tr>
                 <th>Interptetacja BMI</th>
                 <th>Wartość minimalna</th>
                 <th>Wartość maksymalna</th>
             </tr>
+            <?php
+                $query = $db->prepare(" SELECT informacja, wart_min, wart_max FROM bmi");
+                    $query->execute();
+                    $result = $query->get_result();
+                    while($row = $result->fetch_assoc()) {
+                        echo '<tr>';
+                        echo '<td>'.$row['informacja'].'</td>';
+                        echo '<td>'.$row['wart_min'].'</td>';
+                        echo '<td>'.$row['wart_max'].'</td>';
+                        echo '</tr>';
+                    }
+            ?>
             <tr>
                 <td></td>
                 <td></td>
@@ -51,9 +69,30 @@
         <div id="lewy">
             <h2>Podaj wagę i wzrost</h2>
             <form action="bmi.php" metdod="post">
-                Waga: <input type="number" min="1" name="waga">
-                Wzrost: <input type="number" min="1" name="wzrost">
-                <button type="submit">Oblicz i zapamientaj wynik</button>
+                Waga: <input type="number" min="1" name="waga"><br>
+                Wzrost: <input type="number" min="1" name="wzrost"><br>
+                <button type="submit">Oblicz i zapamientaj wynik</button><br>
+                <?php
+                    if(isset($_POST['waga']) && isset($_POST['wzrost'])) {
+                        $waga = $_POST['waga'];
+                        $wzrost = $_POST['wzrost'];
+                        $bmi = $waga / ($wzrost*$wzrost);
+                        $bmi *= 10000;
+                        echo "Twoja waga : $waga; Twój wzrost <br> BMI wynosi: $bmi";
+                        $bmi_id= 0;
+                        if($bmi <=18) $bmi_id = 1;
+                        if($bmi > 19 && $bmi <=25 ) $bmi_id = 2;
+                        if($bmi < 26 && $bmi <= 30) $bmi_id = 3;
+                        if($bmi > 31 && $bmi <= 100) $bmi_id = 4;
+
+                        $dataPomiaru = date("Y-m-d");
+                        
+                        $query = $db-prepare("INSERT INTO wynik (id, bmi_id, data_pomiaru, wynik) 
+                                                VALUES (NULL, '?', '?', '?')");
+                        $query->bind_param("isi", $bmi_id, $dataPomiaru, $bmi);
+                        $query->execute();
+                    }
+                ?>
             </form>
         </div>
 
@@ -63,7 +102,9 @@
 
     <div id="stopka">Autor: 86123019619 <a href="kwerendy.txt">Zobacz kwerendy</a></div>
 
-
+<?php
+    $db->close();
+?>
 
 </body>
 
